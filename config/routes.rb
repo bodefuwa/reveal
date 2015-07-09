fd263311f2dcb935e9c03390945794ca1ee8c871
Bode Fuwa <bodefuwa@gmail.com>
2015-07-09 15:20:56 -0400

    fixed static page routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,14 @@ Rails.application.routes.draw do
   resources :prayer_points
   resources :prayers
   resources :scriptures
-  get 'about'   => 'pages/about'
-  get 'contact' => 'pages/contact'
-  get 'terms'   => 'pages/terms'
-  get 'privacy' => 'pages/privacy'
-  get 'faqs'    => 'pages/faqs'
-  get 'help'    => 'pages/help'
-
   resources :topics
+
+  get 'about'   => 'pages#about'
+  get 'contact' => 'pages#contact'
+  get 'terms'   => 'pages#terms'
+  get 'privacy' => 'pages#privacy'
+  get 'faqs'    => 'pages#faqs'
+  get 'help'    => 'pages#help'
+
   root 'pages#home'
 end
